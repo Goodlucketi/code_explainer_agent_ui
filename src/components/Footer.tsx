f8efdc3c4a72cd06d9b5cc5a536a3bf9f685e6d7
Pistis-Media <goodlucketim94@gmail.com>
2025-08-20 15:23:56 +0100

    Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@ const Footer = () => {
     return (
         <footer className="bg-blue-200 shadow-md p-1 mt-10 fixed bottom-0 left-0 right-0">
             <div className="navbar_container w-10/12 mx-auto py-2 px-2">
-                <div className="logo">
-                    <p className="text-sm text-black">Code Explainer Agent</p>
+                <div className="logo flex justify-between items-center">
+                    <p className="text-sm font-mono">Code Explainer Agent</p>
+                    <p className="text-sm font-mono text-gray-600">Made with ❤️ by Mfoniso Fabian</p>
                 </div>
             </div>
         </footer>
